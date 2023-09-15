c67ee2c21b29316df6968a9801faa73c5344bd9e
Vitali Avagyan <eheva87@gmail.com>
2023-09-15 18:09:39 +0100

    fix: answer yes to install commands

--- steps.sh
+++ steps.sh
@@ -2,7 +2,7 @@
 
 # Add Docker's official GPG key:
 sudo apt-get -y update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -14,5 +14,5 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
